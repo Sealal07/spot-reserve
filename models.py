f195ce4cddae87270070144d52196f4f706fbcf8
Konfermat <semyon.obuhov@gmail.com>
2026-03-17 21:15:04 +0700

    db in progress

--- models.py
+++ models.py
@@ -9,27 +9,29 @@ class Base(DeclarativeBase):
 class User(Base):
     __tablename__ = 'users'
     id: Mapped[int] = mapped_column(primary_key=True)
-    login: Mapped[str] = mapped_column(String(30))
-    email: Mapped[str] = mapped_column(String(30))
+    login: Mapped[str] = mapped_column(String(30), unique=True)
+    email: Mapped[str] = mapped_column(String(30), unique=True)
     hash_password: Mapped[str] = mapped_column(String)
     role: Mapped[str] = mapped_column(String(20))
     # один ко многим (Booking.user_id)
-    booking_list: Mapped[List['Booking']] = relationship()
+    booking_list: Mapped[List['Booking']] = relationship('Booking', back_populates='users_bookingr')
 
 class Spot(Base):
     __tablename__ = 'spots'
     id: Mapped[int] = mapped_column(primary_key=True)
-    number: Mapped[int] = mapped_column(Integer)
+    number: Mapped[int] = mapped_column(Integer, unique=True)
     description: Mapped[str] = mapped_column(String(1000))
     # один ко многим(Booking.spot_id)
-    spot_booking_list: Mapped[List['Booking']]
+    spot_list: Mapped[List['Booking']] = relationship('Booking', back_populates='spot') # spot_list подумать над названием
+    is_active: Mapped[bool] = mapped_column(default=True)
+
 
 class Booking(Base):
     __tablename__ = 'bookings'
     id: Mapped[int] = mapped_column(primary_key=True)
     # users.id
-    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id', ondelete='CASCADE'))
     # spots.id
-    spot_id: Mapped[int] = mapped_column(ForeignKey('spots.id'))
+    spot_id: Mapped[int] = mapped_column(ForeignKey('spots.id', ondelete='CASCADE'))
     start_time: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
     end_time: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
