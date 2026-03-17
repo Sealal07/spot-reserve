6009b355ecbea5d829741e086b82fd72704f83df
alexey1990lbb <blizard90@yandex.ru>
2026-03-17 20:13:03 +0700

    [Task 2] Реализованы эндпоинты: Показ всех столов, удаление и добавление столов только админом

--- routes.py
+++ routes.py
@@ -1,27 +1,47 @@
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, APIRouter, HTTPException
 from sqlalchemy.orm import Session
 from database import User, Spot, get_db
-from sqlalchemy.sql.functions import current_user
+from auth import get_current_user
 
-app = FastAPI(title='Spot Reserve API')
+router = APIRouter()
 
-@app.get('/spots')
+@router.get('/spots')
 def get_all_spots(db: Session = Depends(get_db)):
     '''Получения списка всех столов'''
     db_spots = db.query(Spot).all()
     return db_spots
 
-@app.post('/spots')
-def create_spot(db: Session = Depends(get_db)):
+@router.post('/spots')
+def create_spot(db: Session = Depends(get_db), current_user: User = Depends(get_current_user), number=None, description=None):
     '''Создание стола'''
-    user_admin = db.query(User).filter(User.role == 'admin').first()
-    if not user_admin:
+    # проверяем пользователя на админа
+    if current_user.role != 'admin':
         raise HTTPException(
-            status_code=400,
+            status_code=403,
             detail='Только администратор может добавлять столы!'
-
         )
     else:
         new_spot = Spot(
+            number=number,
+            description=description
 
         )
+        db.add(new_spot)
+        db.commit()
+        return {'Message': 'Стол успешно создан!'}
+
+
+@router.post('/spots/{id}')
+def delete_spot(spot_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    '''Удаление стола'''
+    # проверяем пользователя на админа
+    if current_user.role != 'admin':
+        raise HTTPException(
+            status_code=403,
+            detail='Только администратор может удалять столы!'
+        )
+    else:
+        del_spot = db.query(Spot).filter(Spot.id == spot_id).first()
+        db.delete(del_spot)
+        db.commit()
+        return {'Message': 'Стол успешно удален!'}
