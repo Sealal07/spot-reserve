3f8d0c79bdac4185c468654ca38dcd34377ca04b
Konfermat <semyon.obuhov@gmail.com>
2026-03-17 19:49:29 +0700

    database result

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from typing import Optional, List
-from sqlalchemy import String, Integer
-from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+from sqlalchemy import String, Integer, ForeignKey, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
 
 class Base(DeclarativeBase):
     pass
@@ -8,29 +9,27 @@ class Base(DeclarativeBase):
 class User(Base):
     __tablename__ = 'users'
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(30))
-    surname: Mapped[str] = mapped_column(String(30))
-    patronymic: Mapped[str] = mapped_column(String(30))
-    email: Mapped[str] = mapped_column(String(50))
+    login: Mapped[str] = mapped_column(String(30))
+    email: Mapped[str] = mapped_column(String(30))
     hash_password: Mapped[str] = mapped_column(String)
-    # question 
     role: Mapped[str] = mapped_column(String(20))
-    # один ко многим (Booking.user_booking_id)
-    booking_list: Mapped[List['Booking']] = 
-    booking_limit: Mapped[Optional[int]] = mapped_column(Integer)
+    # один ко многим (Booking.user_id)
+    booking_list: Mapped[List['Booking']] = relationship()
 
 class Spot(Base):
     __tablename__ = 'spots'
     id: Mapped[int] = mapped_column(primary_key=True)
-    pass
+    number: Mapped[int] = mapped_column(Integer)
+    description: Mapped[str] = mapped_column(String(1000))
+    # один ко многим(Booking.spot_id)
+    spot_booking_list: Mapped[List['Booking']]
 
 class Booking(Base):
     __tablename__ = 'bookings'
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_booking_id: Mapped # (users.id)
-
-    spot_id # (spots.id)
-    booked_at
-    expired_at
-
-
+    # users.id
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    # spots.id
+    spot_id: Mapped[int] = mapped_column(ForeignKey('spots.id'))
+    start_time: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    end_time: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
