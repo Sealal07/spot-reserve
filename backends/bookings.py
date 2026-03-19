aee0d1da3f2c577faafc9be84971403de5fd0912
alexey1990lbb <blizard90@yandex.ru>
2026-03-19 19:50:04 +0700

    [Task 3] Сделано

--- backends/bookings.py
+++ backends/bookings.py
@@ -1,35 +1,123 @@
-from anyio import current_effective_deadline
-from fastapi import Depends, APIRouter, HTTPException
+from fastapi import Depends, APIRouter, HTTPException, status
 from sqlalchemy.orm import Session
-from database import User, Spot, Booking, get_db
+from sqlalchemy import and_
+from datetime import datetime
+from pydantic import BaseModel
 
+from database import User, Spot, Booking, get_db
 from auth import get_current_user
 
-router = APIRouter()
+router = APIRouter(prefix="/bookings", tags=["Bookings"])  # перенесла сюда /bookings
 
 
-@router.post('/bookings')
-def create_booking(user_id: int, spot_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
-    '''Создание брони с проверкой доступности'''
-    spot = db.query(Spot).filter(Spot.is_active == True).first()
+# схема для входных данных
+class BookingCreate(BaseModel):
+    spot_id: int
+    start_time: datetime
+    end_time: datetime
+
+
+# схема для ответа
+class BookingResponse(BaseModel):
+    id: int
+    spot_id: int
+    start_time: datetime
+    end_time: datetime
+
+    class Config:
+        from_attributes = True
+
+
+@router.post("/", response_model=BookingResponse)
+def create_booking(
+        booking_data: BookingCreate,
+        db: Session = Depends(get_db),
+        current_user: User = Depends(get_current_user)):
+    """cоздание брони с проверкой доступности по времени"""
+
+    # проверяем, существует ли стол и активен ли он
+    spot = db.query(Spot).filter(Spot.id == booking_data.spot_id, Spot.is_active == True).first()
     if not spot:
+        raise HTTPException(status_code=404, detail="Стол не найден или деактивирован")
+
+    # проверка на пересечение временных интервалов
+    # Формула: (RequestStart < ExistingEnd) AND (RequestEnd > ExistingStart)
+    overlap = db.query(Booking).filter(
+        Booking.spot_id == booking_data.spot_id,
+        and_(
+            booking_data.start_time < Booking.end_time,
+            booking_data.end_time > Booking.start_time
+        )
+    ).first()
+
+    if overlap:
         raise HTTPException(
-            status_code=404,
-            detail='Стол не найден или уже забронирован!'
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Этот стол уже забронирован на выбранное время"
         )
-    if spot:
-        new_booking = Booking(
-            user_id=current_user.id,
-            spot_id=spot_id,
-            is_active=False,
-        )
-        db.add(new_booking)
-        db.commit()
-        db.refresh(new_booking)
 
-        return {'Message': 'Стол успешно забронирован!'}
+    # создание записи
+    new_booking = Booking(
+        user_id=current_user.id,
+        spot_id=booking_data.spot_id,
+        start_time=booking_data.start_time,
+        end_time=booking_data.end_time
+    )
+
+    db.add(new_booking)
+    db.commit()
+    db.refresh(new_booking)
+    return new_booking
 
 
-@router.get('/bookings/my')
-def get_my_bookings(user_id: int, db: Session = Depends(get_db)):
+@router.get('/my')
+def get_my_bookings(user_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     '''Список своих текущих и прошедших бронирований'''
+    my_reservations = db.query(Booking).filter(Booking.id == user_id).all()
+    if current_user:
+        if not my_reservations:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='У вас отсутствуют забронированные столики'
+            )
+
+    return my_reservations
+
+@router.get("/")
+def get_all_bookings_for_admin(db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    '''Получение всех броней админом'''
+    if current_user.role != 'admin':
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='Только администратор может просмотреть все брони'
+        )
+    else:
+        all_bookings = db.query(Booking).all()
+        if not all_bookings:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='Брони отсутствуют'
+            )
+        return all_bookings
+
+
+@router.delete('/{id}')
+def delete_booking(id:int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    '''Отмена брони для юзера и удаление брони для админа'''
+    if not current_user:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='Вы не можете отменить чужие брони'
+        )
+    if current_user.role == 'admin':
+        db.query(Booking).filter(Booking.id == id).delete()
+        db.commit()
+        return {'message': 'Успешно удалено'}
+
+    db.query(Booking).filter(Booking.user_id == current_user.id, Booking.id == id).delete()
+    db.commit()
+    return {'message': 'Успешно удалено'}
+
+
+
+
