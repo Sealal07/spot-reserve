3036f9cef6600c2fe2992c93bf193ecbdb4ad375
alexey1990lbb <blizard90@yandex.ru>
2026-03-24 21:16:20 +0700

    [Task 4] Добавил проверку времени бронирования. Проверки: существование брони, удаление админом и пользователем

--- backend/bookings.py
+++ backend/bookings.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, APIRouter, HTTPException, status
 from sqlalchemy.orm import Session
 from sqlalchemy import and_
-from datetime import datetime
+from datetime import datetime, timedelta
 from pydantic import BaseModel
 
 from database import User, Spot, Booking, get_db
@@ -34,46 +34,65 @@ def create_booking(
         db: Session = Depends(get_db),
         current_user: User = Depends(get_current_user)):
     """создание брони с проверкой доступности по времени"""
+    # Добавим проверку на время бронирования
+    time_now = datetime.now()
+    start_time = datetime.strptime(booking_data['start_time'], '%Y-%m-%d %H:%M')
+    end_time = datetime.strptime(booking_data['end_time'], '%Y-%m-%d %H:%M')
+    total_seconds_booking = (end_time - start_time).total_seconds() # получаем количество секунд бронирования
+    max_hour_booking = 60 * 60 * 12 # это максимальное время бронирование в секундах!!!!
 
-    # проверяем, существует ли стол и активен ли он
-    spot = db.query(Spot).filter(Spot.id == booking_data.spot_id, Spot.is_active == True).first()
-    if not spot:
-        raise HTTPException(status_code=404, detail="Стол не найден или деактивирован")
+    try:
+        if start_time < time_now:
+            raise HTTPException(status_code=400, detail='Время\дата начала бронирования не может быть меньше текущего времени\даты!')
+        elif end_time <= start_time:
+            raise HTTPException(status_code=400, detail='Время\дата завершения бронирования должна быть больше чем время\дата начала бронирования!')
+        elif total_seconds_booking > max_hour_booking:
+             raise HTTPException(status_code=400, detail='Время бронирования не может превышать 12 часов!')
+        else:
 
-    # проверка на пересечение временных интервалов
-    # Формула: (RequestStart < ExistingEnd) AND (RequestEnd > ExistingStart)
-    overlap = db.query(Booking).filter(
-        Booking.spot_id == booking_data.spot_id,
-        and_(
-            booking_data.start_time < Booking.end_time,
-            booking_data.end_time > Booking.start_time
-        )
-    ).first()
+            # проверяем, существует ли стол и активен ли он
+            spot = db.query(Spot).filter(Spot.id == booking_data.spot_id, Spot.is_active == True).first()
+            if not spot:
+                raise HTTPException(status_code=404, detail="Стол не найден или деактивирован")
 
-    if overlap:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Этот стол уже забронирован на выбранное время"
-        )
+            # проверка на пересечение временных интервалов
+            # Формула: (RequestStart < ExistingEnd) AND (RequestEnd > ExistingStart)
+            overlap = db.query(Booking).filter(
+                Booking.spot_id == booking_data.spot_id,
+                and_(
+                    booking_data.start_time < Booking.end_time,
+                    booking_data.end_time > Booking.start_time
+                )
+            ).first()
 
-    # создание записи
-    new_booking = Booking(
-        user_id=current_user.id,
-        spot_id=booking_data.spot_id,
-        start_time=booking_data.start_time,
-        end_time=booking_data.end_time
-    )
+            if overlap:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="Этот стол уже забронирован на выбранное время"
+                )
 
-    db.add(new_booking)
-    db.commit()
-    db.refresh(new_booking)
-    return new_booking
+            # создание записи
+            new_booking = Booking(
+                user_id=current_user.id,
+                spot_id=booking_data.spot_id,
+                start_time=booking_data.start_time,
+                end_time=booking_data.end_time
+            )
+
+            db.add(new_booking)
+            db.commit()
+            db.refresh(new_booking)
+            return new_booking
+    except HTTPException as e:
+        return {'Message': f'Error: {e}'}
 
 
 @router.get('/my')
 def get_my_bookings(user_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     '''Список своих текущих и прошедших бронирований'''
     my_reservations = db.query(Booking).filter(Booking.user_id == current_user.id).all()
+
+    # проверка на существование бронирований
     if current_user:
         if not my_reservations:
             raise HTTPException(
@@ -103,19 +122,26 @@ def get_all_bookings_for_admin(db: Session = Depends(get_db), current_user: User
 @router.delete('/{id}')
 def delete_booking(id:int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     '''Отмена брони для юзера и удаление брони для админа'''
-    if not current_user:
+    # проверка на существование бронирования
+    del_booking = db.query(Booking).filter(Booking.id == id)
+    if not del_booking:
+        raise HTTPException(status_code=404, detail='Бронирование не найдено!')
+    # проверка на удаление другим пользователем
+    if not current_user or Booking.user_id != current_user.id:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
-            detail='Вы не можете отменить чужие брони'
+            detail='Вы не можете отменять чужие брони'
         )
-    if current_user.role == 'admin':
+    # проверка удаление админом и пользователем, кому принадлежит бронь
+    if current_user.role == 'admin' or Booking.user_id == current_user.id:
         db.query(Booking).filter(Booking.id == id).delete()
         db.commit()
-        return {'message': 'Успешно удалено'}
-
-    db.query(Booking).filter(Booking.user_id == current_user.id, Booking.id == id).delete()
-    db.commit()
     return {'message': 'Успешно удалено'}
+
+    # убираем, т.к. теперь есть проверка на пользователя(выше)
+    # db.query(Booking).filter(Booking.user_id == current_user.id, Booking.id == id).delete()
+    # db.commit()
+    # return {'message': 'Успешно удалено'}
 
 
 
