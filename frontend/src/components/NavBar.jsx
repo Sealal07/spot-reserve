0e39547861024ad679739ff844f3f2053c8f165f
Denis Markov <da.marckov2015@yandex.ru>
2026-03-24 21:14:22 +0700

    task 4: add сÐclose buttons

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 
 function NavBar() {
+  const [isAuth, setIsAuth] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+    if (token) {
+      setIsAuth(true);
+    }
+  },[])
+  
+  useEffect(() => {
+    localStorage.removeItem('token');
+    setIsAuth(false);
+  }, [])
+
   return (
     <nav>
       <div>
@@ -9,20 +23,31 @@ function NavBar() {
       </div>
       
       <ul>
+        {isAuth ? (
+          <>
         <li>
           <Link to="/calendar">Календарь</Link>
         </li>
+        <li>
+          <Link to="/my">Личный кабинет</Link>
+        </li>
+        <li>
+          <Link to="/logout">Выйти</Link>
+        </li>
+          </>
+
+        ) :
+        (
+          <>
         <li>
           <Link to="/login">Войти</Link>
         </li>
         <li>
           <Link to="/register">Регистрация</Link>
         </li>
+          </>
+        )}
 
-        <li>
-          <Link to="/my">Личный кабинет</Link>
-
-        </li>
       </ul>
     </nav>
   );
