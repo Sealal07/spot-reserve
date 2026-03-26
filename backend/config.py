ab443e8aa6e9ba9a08de5deb4d03bd9eb5ea0926
Alex <118180019+Sealal07@users.noreply.github.com>
2026-03-26 18:01:19 +0700

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -4,4 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 ALGORITHM = os.getenv('ALGORITHM')
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
+raw_expire = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
+
+if raw_expire is not None:
+    ACCESS_TOKEN_EXPIRE_MINUTES = int(raw_expire)
+else:
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
