c08844c6e1908c7f7ea54a8b993deb69f3129409
Александра Рудак <rudak_a@ItTopNSK.local>
2026-03-26 21:16:11 +0700

    fix encoding files (hash password)

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-SECRET_KEY = os.getenv('SECRET_KEY')
-ALGORITHM = os.getenv('ALGORITHM')
+SECRET_KEY = os.getenv('SECRET_KEY') or "super_secret_key"
+ALGORITHM = os.getenv('ALGORITHM') or "HS256"
 raw_expire = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
 
 if raw_expire is not None:
