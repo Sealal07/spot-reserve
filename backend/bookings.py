68bf56b16ae362a00f0b6189efeef813b2aff7dc
Alex <118180019+Sealal07@users.noreply.github.com>
2026-03-26 18:05:03 +0700

    Update bookings.py

--- backend/bookings.py
+++ backend/bookings.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import Session
 from sqlalchemy import and_
 from datetime import datetime, timedelta
 from pydantic import BaseModel
-
-from database import User, Spot, Booking, get_db
-from auth import get_current_user
+from .models import User, Spot, Booking
+from .engine import  get_db
+from .auth import get_current_user
 
 router = APIRouter(prefix="/bookings", tags=["Bookings"])  # перенесла сюда /bookings
 
@@ -34,20 +34,21 @@ def create_booking(
         db: Session = Depends(get_db),
         current_user: User = Depends(get_current_user)):
     """создание брони с проверкой доступности по времени"""
-    # Добавим проверку на время бронирования
     time_now = datetime.now()
     start_time = datetime.strptime(booking_data['start_time'], '%Y-%m-%d %H:%M')
     end_time = datetime.strptime(booking_data['end_time'], '%Y-%m-%d %H:%M')
-    total_seconds_booking = (end_time - start_time).total_seconds() # получаем количество секунд бронирования
-    max_hour_booking = 60 * 60 * 12 # это максимальное время бронирование в секундах!!!!
+    total_seconds_booking = (end_time - start_time).total_seconds()  # получаем количество секунд бронирования
+    max_hour_booking = 60 * 60 * 12  # это максимальное время бронирование в секундах!!!!
 
     try:
         if start_time < time_now:
-            raise HTTPException(status_code=400, detail='Время\дата начала бронирования не может быть меньше текущего времени\даты!')
+            raise HTTPException(status_code=400,
+                                detail='Время\дата начала бронирования не может быть меньше текущего времени\даты!')
         elif end_time <= start_time:
-            raise HTTPException(status_code=400, detail='Время\дата завершения бронирования должна быть больше чем время\дата начала бронирования!')
+            raise HTTPException(status_code=400,
+                                detail='Время\дата завершения бронирования должна быть больше чем время\дата начала бронирования!')
         elif total_seconds_booking > max_hour_booking:
-             raise HTTPException(status_code=400, detail='Время бронирования не может превышать 12 часов!')
+            raise HTTPException(status_code=400, detail='Время бронирования не может превышать 12 часов!')
         else:
 
             # проверяем, существует ли стол и активен ли он
@@ -78,21 +79,20 @@ def create_booking(
                 start_time=booking_data.start_time,
                 end_time=booking_data.end_time
             )
+        db.add(new_booking)
+        db.commit()
+        db.refresh(new_booking)
+        return new_booking
 
-            db.add(new_booking)
-            db.commit()
-            db.refresh(new_booking)
-            return new_booking
     except HTTPException as e:
         return {'Message': f'Error: {e}'}
+
 
 
 @router.get('/my')
 def get_my_bookings(user_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     '''Список своих текущих и прошедших бронирований'''
     my_reservations = db.query(Booking).filter(Booking.user_id == current_user.id).all()
-
-    # проверка на существование бронирований
     if current_user:
         if not my_reservations:
             raise HTTPException(
@@ -122,7 +122,6 @@ def get_all_bookings_for_admin(db: Session = Depends(get_db), current_user: User
 @router.delete('/{id}')
 def delete_booking(id:int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     '''Отмена брони для юзера и удаление брони для админа'''
-    # проверка на существование бронирования
     del_booking = db.query(Booking).filter(Booking.id == id)
     if not del_booking:
         raise HTTPException(status_code=404, detail='Бронирование не найдено!')
@@ -130,15 +129,13 @@ def delete_booking(id:int, db: Session = Depends(get_db), current_user: User = D
     if not current_user or Booking.user_id != current_user.id:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
-            detail='Вы не можете отменять чужие брони'
+            detail='Вы не можете отменить чужие брони'
         )
-    # проверка удаление админом и пользователем, кому принадлежит бронь
     if current_user.role == 'admin' or Booking.user_id == current_user.id:
         db.query(Booking).filter(Booking.id == id).delete()
         db.commit()
-    return {'message': 'Успешно удалено'}
+        return {'message': 'Успешно удалено'}
 
-    # убираем, т.к. теперь есть проверка на пользователя(выше)
     # db.query(Booking).filter(Booking.user_id == current_user.id, Booking.id == id).delete()
     # db.commit()
     # return {'message': 'Успешно удалено'}
